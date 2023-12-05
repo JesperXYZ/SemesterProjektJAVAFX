f6b9f0eecc94ff0f7c545f1c46d601d3a2641523
JesperXYZ <jesper@bondesen.dk>
2023-12-05 08:45:37 +0100

    Commit GameLauncher og RoomController

--- SemesterProjektJAVAFX/src/main/java/semesterprojektjavafx/semesterprojektjavafx/presentation/GameLauncher.java
+++ SemesterProjektJAVAFX/src/main/java/semesterprojektjavafx/semesterprojektjavafx/presentation/GameLauncher.java
@@ -18,19 +18,27 @@ import java.io.IOException;
 public class GameLauncher extends Application {
 
     private static final String HOUSEENTRY_FILE = "/semesterprojektjavafx/semesterprojektjavafx/houseEntry.fxml";
-    private static final String CORRIDOR_FILE = "/semesterprojektjavafx/semesterprojektjavafx/corridor.fxml";
-    private static final String GARDEN_FILE = "/semesterprojektjavafx/semesterprojektjavafx/garden.fxml";
-    private static final String BEDROOM_FILE = "/semesterprojektjavafx/semesterprojektjavafx/bedroom.fxml";
-    private static final String KITCHEN_FILE = "/semesterprojektjavafx/semesterprojektjavafx/kitchen.fxml";
-    private static final String CLUB_FILE = "/semesterprojektjavafx/semesterprojektjavafx/club.fxml";
-    private static final String BIRTHDAYPARTY_FILE = "/semesterprojektjavafx/semesterprojektjavafx/birthdayParty.fxml";
-    private static final String SCHOOL_FILE = "/semesterprojektjavafx/semesterprojektjavafx/school.fxml";
-    private static final String FOOTBALL_FILE = "/semesterprojektjavafx/semesterprojektjavafx/football.fxml";
 
+    Stage stage;
+    Scene scene;
+    Parent root;
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        root = (Parent) FXMLLoader.load(getClass().getResource(HOUSEENTRY_FILE));
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        /*game = FXMLLoader.load(getClass().getResource(HOUSEENTRY_FILE));
+        game.show();*/
+    }
+    public static void main(String[] args){
+        launch(args);
+    }
+    /*
     @FXML
     Stage game = new Stage();
-    /*@FXML
+    @FXML
     Parent parent = new Parent() {
         @Override
         protected ObservableList<Node> getChildren() {
@@ -42,7 +50,7 @@ public class GameLauncher extends Application {
             corridor = new SubScene(parent,600,400),
             bedroom = new SubScene(parent,600,400),
             houseEntry = new SubScene(parent,600,400),
-            garden = new SubScene(parent,600,400);*/
+            garden = new SubScene(parent,600,400);
 
     @FXML
     Pane kitchenPane = new Pane();
@@ -105,93 +113,5 @@ public class GameLauncher extends Application {
         if(keyCode==KeyCode.KP_DOWN){
             goDown();
         }
-    }
-
-    Stage stage;
-    Scene scene;
-    Parent root;
-    @FXML
-    void goCorridor(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource(CORRIDOR_FILE));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-    @FXML
-    void goHouseEntry(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource(HOUSEENTRY_FILE));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-    @FXML
-    void goGarden(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource(GARDEN_FILE));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-    @FXML
-    void goKitchen(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource(KITCHEN_FILE));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-    @FXML
-    void goBedroom(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource(BEDROOM_FILE));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-    @FXML
-    void goClub(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource(CLUB_FILE));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-    @FXML
-    void goBirthdayParty(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource(BIRTHDAYPARTY_FILE));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-    @FXML
-    void goSchool(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource(SCHOOL_FILE));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-    @FXML
-    void goFootball(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource(FOOTBALL_FILE));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-    @Override
-    public void start(Stage stage) throws Exception {
-        root = (Parent) FXMLLoader.load(getClass().getResource(HOUSEENTRY_FILE));
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-        /*game = FXMLLoader.load(getClass().getResource(HOUSEENTRY_FILE));
-        game.show();*/
-    }
-    public static void main(String[] args){
-        launch(args);
-    }
+    }*/
 }
--- SemesterProjektJAVAFX/src/main/java/semesterprojektjavafx/semesterprojektjavafx/presentation/RoomController.java
+++ SemesterProjektJAVAFX/src/main/java/semesterprojektjavafx/semesterprojektjavafx/presentation/RoomController.java
@@ -1,4 +1,99 @@
 package semesterprojektjavafx.semesterprojektjavafx.presentation;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class RoomController {
+    private static final String HOUSEENTRY_FILE = "/semesterprojektjavafx/semesterprojektjavafx/houseEntry.fxml";
+    private static final String CORRIDOR_FILE = "/semesterprojektjavafx/semesterprojektjavafx/corridor.fxml";
+    private static final String GARDEN_FILE = "/semesterprojektjavafx/semesterprojektjavafx/garden.fxml";
+    private static final String BEDROOM_FILE = "/semesterprojektjavafx/semesterprojektjavafx/bedroom.fxml";
+    private static final String KITCHEN_FILE = "/semesterprojektjavafx/semesterprojektjavafx/kitchen.fxml";
+    private static final String CLUB_FILE = "/semesterprojektjavafx/semesterprojektjavafx/club.fxml";
+    private static final String BIRTHDAYPARTY_FILE = "/semesterprojektjavafx/semesterprojektjavafx/birthdayParty.fxml";
+    private static final String SCHOOL_FILE = "/semesterprojektjavafx/semesterprojektjavafx/school.fxml";
+    private static final String FOOTBALL_FILE = "/semesterprojektjavafx/semesterprojektjavafx/football.fxml";
+
+    Stage stage;
+    Scene scene;
+    Parent root;
+    @FXML
+    void goCorridor(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource(CORRIDOR_FILE));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    void goHouseEntry(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource(HOUSEENTRY_FILE));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    void goGarden(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource(GARDEN_FILE));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    void goKitchen(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource(KITCHEN_FILE));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    void goBedroom(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource(BEDROOM_FILE));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    void goClub(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource(CLUB_FILE));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    void goBirthdayParty(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource(BIRTHDAYPARTY_FILE));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    void goSchool(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource(SCHOOL_FILE));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    void goFootball(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource(FOOTBALL_FILE));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
